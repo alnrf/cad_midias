14efb07e0fdf0723f0a1cfdb17da449ee5c2f076
Allan <allanrf85@gmail.com>
2019-10-08 15:20:30 -0300

    COmmit

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,56 +1,67 @@
-import React from 'react';
-import { firestoreConnect } from 'react-redux-firebase';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React from "react";
+import { firestoreConnect } from "react-redux-firebase";
+import { connect } from "react-redux";
+import { compose } from "redux";
 
-const ProjectDetails = (props) => {
-    const { project } = props;
-    if (project) {
-        return (
-            <div className="container section project-details">
-            <div className="card z-depth-0">
-              <div className="card-content">
-                <span className="card-title"><b>Título Nacional: {project.title_pt}</b></span>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-               
-                <p><b>Título Internacional:</b> {project.title_eng}</p>
-                <p><b>Categoria:</b> {project.category}</p>
-                <p><b>Mídia:</b> {project.media_type}</p>
-                <p><b>Ano de Lançamento:</b> {project.year}</p>
-                <p><b>Volume/Temporada:</b> {project.season}</p>
-                <p><b>Embalagem:</b> {project.package}</p>
-            
-                <div className="image" >
-                <img src={project.cover} width= '200px' alt=''/>
-                
-                </div>
-              </div>
+const ProjectDetails = props => {
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+    
+
+          <div className="card horizontal grey lighten-4 black-text">
+            <div className="card-image">
+              <img src={project.cover} height="300px" alt="" />
+            </div>
+       
+            <div className="card-content grey lighten-4 black-text">
+            <h1 className="card-title"><b>{project.title_pt}</b></h1>
+              <p>
+                <b>Título Internacional:</b> {project.title_eng}
+              </p>
+              <p>
+                <b>Categoria:</b> {project.category}
+              </p>
+              <p>
+                <b>Mídia:</b> {project.media_type}
+              </p>
+              <p>
+                <b>Ano de Lançamento:</b> {project.year}
+              </p>
+              <p>
+                <b>Volume/Temporada:</b> {project.season}
+              </p>
+              <p>
+                <b>Embalagem:</b> {project.package}
+              </p>
             </div>
           </div>
-        )
-    } else {
-        return (
-            <div className='container center'>
-                <p>Dados não existem</p>
-            </div>
-        )
-    }
-}
+        </div>
+      
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Dados não existem</p>
+      </div>
+    );
+  }
+};
 
 const mapStateToProps = (state, ownProps) => {
-    // console.log(state);
-    const id = ownProps.match.params.id;
-    const projects = state.firestore.data.media;
-    const project = projects ? projects[id] : null
-    return {
-        project: project
-    }
-}
+  // console.log(state);
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.media;
+  const project = projects ? projects[id] : null;
+  return {
+    project: project
+  };
+};
 
 export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([
-        { collection: 'media' }
+  connect(mapStateToProps),
+  firestoreConnect([
+      { collection: "media" }
     ])
 )(ProjectDetails);
--- src/store/reducers/projectReducers.js
+++ src/store/reducers/projectReducers.js
@@ -1,27 +1,5 @@
 const initState = {
-  projects: [
-    {
-      id: '1',
-      title_pt: 'Guardiões da Galáxia',
-      title_eng: 'Guardians of The Galaxy',
-      category: 'Filme',
-      media_type: 'Blu-ray 3D, Blu-ray, DVD'
-    },
-    {
-      id: '2',
-      title_pt: 'Vingadores: Gerra Infinita',
-      title_eng: 'Avengers: Infinity War',
-      category: 'Filme',
-      media_type: 'Blu-ray 3D, Blu-ray, DVD'
-    },
-    {
-      id: '3',
-      title_pt: 'Vingadores: Ultimato',
-      title_eng: 'Avengers: End Game',
-      category: 'Filme',
-      media_type: 'Blu-ray 3D, Blu-ray, DVD'
-    }
-  ]
+  
 }
 
 const projectReducer = (state = initState, action) => {
