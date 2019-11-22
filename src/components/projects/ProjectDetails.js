2993db952f8fdfc09cd2407ad561adebca547700
Allan <allanrf85@gmail.com>
2019-11-22 13:38:27 -0300

    Commit

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -8,37 +8,35 @@ const ProjectDetails = props => {
   if (project) {
     return (
       <div className="container section project-details">
-    
-
-          <div className="card horizontal grey lighten-4 black-text">
-            <div className="card-image">
-              <img src={project.cover} height="300px" alt="" />
-            </div>
-       
-            <div className="card-content grey lighten-4 black-text">
-            <h1 className="card-title"><b>{project.title_pt}</b></h1>
-              <p>
-                <b>Título Internacional:</b> {project.title_eng}
-              </p>
-              <p>
-                <b>Categoria:</b> {project.category}
-              </p>
-              <p>
-                <b>Mídia:</b> {project.media_type}
-              </p>
-              <p>
-                <b>Ano de Lançamento:</b> {project.year}
-              </p>
-              <p>
-                <b>Volume/Temporada:</b> {project.season}
-              </p>
-              <p>
-                <b>Embalagem:</b> {project.package}
-              </p>
-            </div>
+        <div className="card horizontal grey lighten-4 black-text">
+          <div className="card-image">
+            <img src={project.cover} height="300px" alt="" />
+          </div>
+          <div className="card-content grey lighten-4 black-text">
+            <h1 className="card-title">
+              <b>{project.title_pt}</b>
+            </h1>
+            <p>
+              <b>Título Internacional:</b> {project.title_eng}
+            </p>
+            <p>
+              <b>Categoria:</b> {project.category}
+            </p>
+            <p>
+              <b>Mídia:</b> {project.media_type}
+            </p>
+            <p>
+              <b>Ano de Lançamento:</b> {project.year}
+            </p>
+            <p>
+              <b>Volume/Temporada:</b> {project.season}
+            </p>
+            <p>
+              <b>Embalagem:</b> {project.package}
+            </p>
           </div>
         </div>
-      
+      </div>
     );
   } else {
     return (
@@ -61,7 +59,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([
-      { collection: "media" }
-    ])
+  firestoreConnect([{ collection: "media" }])
 )(ProjectDetails);
